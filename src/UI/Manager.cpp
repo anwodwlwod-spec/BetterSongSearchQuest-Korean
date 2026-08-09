3f6b1e4bddae0f1db100b60ee91f93fb57825ae7
anwodwlwod-spec <anwodwlwod@gmail.com>
2026-08-09 18:52:29 +0900

    Translate Better Song Search menu name

--- src/UI/Manager.cpp
+++ src/UI/Manager.cpp
@@ -21,7 +21,7 @@ using namespace GlobalNamespace;
 
 void BetterSongSearch::UI::Manager::Init() {
     // Register the menu button
-    BSML::Register::RegisterMenuButton("Better Song Search", "더 나은 곡 검색", [this]() {
+    BSML::Register::RegisterMenuButton("향상된 곡 검색", "향상된 곡 검색", [this]() {
         DEBUG("MenuButtonClick");
         ShowFlow(false);
     });
@@ -50,92 +50,18 @@ custom_types::Helpers::Coroutine BetterSongSearch::UI::Manager::Debug() {
         // Tests for the fcinstance
         if (fcInstance) {
             DEBUG("Iteration: {}", iterations_count);
-            //     // Select random song go into the play menu and go back
             {
                 auto songlist = fcInstance->SongListController;
                 co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(0.01f));
                 songlist->SelectRandom();
                 auto currentSong = songlist->GetCurrentSong();
                 if (currentSong) {
-                    // co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(0.1f));
-
-                    // // Play song and go back
-                    // if (songlist->playButton->get_gameObject()->get_active()) {
-                    //     songlist->PlaySong(currentSong);
-                    //     co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(2.5f));
-                    //     // Press back button
-                    //     UnityEngine::GameObject::Find("BackButton")->GetComponent<HMUI::NoTransitionsButton*>()->Press();
-                    // }
-
                     ++iterations_count;
                 } else {
                     WARNING("Current song is null, skipping");
                 }
             }
-
-            // Pick random song, show details, close details
-            // {
-            //     auto songlist = fcInstance->SongListController;
-            //     co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(0.1f));
-            //     songlist->SelectRandom();
-            //     auto currentSong = songlist->GetCurrentSong();
-            //     if (currentSong) {
-            //         co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(0.1f));
-            //         songlist->ShowSongDetails();
-            //         co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(0.1f));
-            //         songlist->uploadDetailsModal->CloseModal();
-            //         ++iterations_count;
-            //     }
-            // }
         }
-
-        // Debug restart of the game
-        // {
-        //     DEBUG("Open BSS");
-
-        //     DEBUG("Press back button");
-        //     // Press back button
-        //     UnityEngine::GameObject::Find("BackButton")->GetComponent<HMUI::NoTransitionsButton*>()->Press();
-
-        //     co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(3.0f));
-
-        //     DEBUG("Reload the game manually");
-        //     // Reload the game manually
-        //     UnityEngine::Resources::FindObjectsOfTypeAll<MenuTransitionsHelper*>()[0]->RestartGame(nullptr);
-        //     co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(5.0f));
-        // }
-
-        // Select current song, go into song list and back
-        // {
-        //     co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(0.5f));
-        //     // this->SelectRandom();
-        //     if (this->currentSong) {
-        //         co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(0.5f));
-        //         this->PlaySong(this->currentSong);
-        //         co_yield reinterpret_cast<System::Collections::IEnumerator*>(UnityEngine::WaitForSeconds::New_ctor(1.5f));
-
-        //         // Press back button
-        //         UnityEngine::GameObject::Find("BackButton")->GetComponent<HMUI::NoTransitionsButton*>()->Press();
-        //         // manager.ShowFlow(true);
-
-        //         ++iterations_count;
-        //     }
-        // }
-
-        // GC testing
-        // {
-        //     System::GC::Collect();
-
-        //     int64_t mem = UnityEngine::Profiling::Profiler::GetMonoUsedSizeLong();
-        //     DEBUG(
-        //         "\nIter {}\nUsed {}\nChan {}\nInit: {}",
-        //         iterations_count,
-        //         pretty_bytes(mem),
-        //         pretty_bytes(mem - lastused),
-        //         pretty_bytes(lastused - initialmemusage)
-        //     );
-        //     lastused = mem;
-        // }
     }
     co_yield nullptr;
 }
@@ -157,7 +83,6 @@ void BetterSongSearch::UI::Manager::ShowFlow(bool immediately) {
         flow = BSML::Helpers::CreateFlowCoordinator<BetterSongSearch::UI::FlowCoordinators::BetterSongSearchFlowCoordinator*>();
     }
     parentFlow = BSML::Helpers::GetMainFlowCoordinator()->YoungestChildFlowCoordinatorOrSelf();
-    // TODO: Recover in case parentFlow is null
     parentFlow->PresentFlowCoordinator(flow.ptr(), nullptr, HMUI::ViewController::AnimationDirection::Horizontal, false, false);
 }
 
